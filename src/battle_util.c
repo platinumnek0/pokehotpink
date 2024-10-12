3fe24330eb99ef5dfc8d746a8445da9781cd34b1
platinumnek0 <soggynumbertwo@gmail.com>
2024-10-12 14:35:44 -0400

    updated corrosion's deal

--- src/battle_util.c
+++ src/battle_util.c
@@ -10124,6 +10124,14 @@ static inline void MulByTypeEffectiveness(uq4_12_t *modifier, u32 move, u32 move
         if (recordAbilities)
             RecordAbilityBattle(battlerAtk, abilityAtk);
     }
+    else if ( moveType == TYPE_POISON && defType == TYPE_STEEL
+            && abilityAtk == ABILITY_CORROSION 
+            && mod == UQ_4_12(0.0))
+            {
+                mod = UQ_4_12(2.0);
+                if (recordAbilities)
+                    RecordAbilityBattle(battlerAtk, abilityAtk);
+            }
 
     if (moveType == TYPE_PSYCHIC && defType == TYPE_DARK && gStatuses3[battlerDef] & STATUS3_MIRACLE_EYED && mod == UQ_4_12(0.0))
         mod = UQ_4_12(1.0);
